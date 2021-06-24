09a46d514943b6586211e3276d0121cdb7c2908d
manjeetdhayal <76167344+manjeetdhayal@users.noreply.github.com>
2021-06-24 15:23:19 +0530

    functioning SCSS not added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App=()=>{
+  const [ip, setIp]= useState([]); 
+  const [query, setQuery] = useState("8.8.8.8");
+  const [search, setSearch] = useState(""); 
+  
+  useEffect(() => {
+    getIp();
+    
+  }, [query]);
+  const getIp= async () => {
+    const response = await fetch(`https://ipapi.co/${query}/json/`)
+    .then(function(response) {
+      response.json().then(jsonData => {
+        console.log(jsonData);
+        setIp(jsonData);
+
+        console.log(Array.isArray(jsonData));
+      });
+      
+     
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
+  }
+  
+  const updateSearch = (e) =>{
+     setSearch(e.target.value); 
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault(); 
+    setQuery(search); 
+    setSearch('');
+  };
+
+  return(
+    <div className="container">
+       <form onSubmit={getSearch} className="search-form" >
+         <input className = " search-bar" 
+         type= "text" 
+         value = {search} 
+         onChange = {updateSearch} 
+         />
+         <button className ="search-button" type = "submit" > Search </button>
+
+       </form>
+   
+   <div> {ip.ip} </div> 
+   <div> {ip.version} </div>
+   <div>  {ip.city} </div>
+   <div> {ip.asn}</div>
+   <div> {ip.continent_code} </div>
+   <div> {ip.country} </div>
+   <div> {ip.country_area}</div>
+   <div> {ip.country_calling_code}</div>
+   <div> {ip.country_capital}</div>
+   <div> {ip.country_code_iso3}</div>
+   <div> {ip.country_name}</div>
+
+   <div> {ip.country_population}</div>
+   <div> {ip.currency}</div>
+   <div> {ip.currency_name}</div>
+   <div> {ip.languages}</div>
+   <div> {ip.region}</div>
+   <div> {ip.region_code}</div>
+   <div> {ip.timezone}</div>
+   <div> {ip.utc_offset}</div>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
